1a99b1ea0ff182689f8bd44f0d975349932c3984
Matheus Estevam Borgmann <83619728+matheusborgmann@users.noreply.github.com>
2022-05-31 13:53:53 -0300

    Update Main.sql

--- Main.sql
+++ Main.sql
@@ -100,22 +100,22 @@ INSERT INTO ProfTurma (AnoSem, CodDepto, NumDisc, SiglaTur, CodProf)
 VALUES ("1", 1, 1, "POO", 1), ("2", 2, 2, "BD", 2), ("1", 3, 3, "ED", 3);
 INSERT INTO Titulacao (CodTit, NomeTit)
 VALUES (1, "VNL"), (2, "ADC"), (3, "ADN");
-/b) Obtenha os nomes das disciplinas que possuem mais de três créditos./
+/*b) Obtenha os nomes das disciplinas que possuem mais de três créditos.*/
 SELECT * FROM Disciplina WHERE CreditosDisc > 3;
-/c) Obtenha os códigos dos professores que ministraram aulas em 2002/2./	
+/*c) Obtenha os códigos dos professores que ministraram aulas em 2002/2./	
 
-/ou/
+/ou*/
 SELECT CodProf FROM ProfTurma WHERE AnoSem = '2002/2';
 /*d) Obtenha os nomes das disciplinas seguidas do nome de seu departamento. */
 SELECT NomeDisc, CodDepto FROM Disciplina;
-/e) Obtenha os nomes dos professores que possuem titulacao de ‘DOUTOR/    
+/*e) Obtenha os nomes dos professores que possuem titulacao de ‘DOUTOR*/    
 SELECT NomeProf FROM Professor where CodTit = '5';
 /*f) Obtenha os nomes dos professores que ministraram aulas em 2002/2. */	
 SELECT CodProf FROM ProfTurma WHERE AnoSem = '2002/2';
 SELECT NomeProf FROM Professor WHERE CodProf in (1, 2);
 /*g) Obtenha os numeros das salas do predio de nome ’LABORATORIOS’ cuja capacidade seja maior que 30. */
      SELECT NumSala FROM Sala WHERE CodPred = 1001 and CapacSala > 30;
-/h) Obtenha os nomes das disciplinas que foram oferecidas em 2002/2/	
+/*h) Obtenha os nomes das disciplinas que foram oferecidas em 2002/2*/	
      SELECT NumDisc FROM Turma WHERE AnoSem = '2002/2';
 SELECT NumDisc FROM Disciplina WHERE NumDisc in (5, 6);
 /*i) Obtenha os números das salas do prédio "LABORATÓRIOS". */
@@ -132,6 +132,6 @@ SELECT CodProf FROM ProfTurma WHERE AnoSem NOT IN ('2002/2');
 /*m) Obtenha os códigos dos professores que ministraram aulas em 2002/1 e 2002/2.
 */ 
 SELECT CodProf FROM ProfTurma WHERE AnoSem = '2002/2' or AnoSem = '2002/1';
-/n) Obtenha os nomes dos departamentos em que há pelo menos uma disciplina com mais que três créditos./
+/*n) Obtenha os nomes dos departamentos em que há pelo menos uma disciplina com mais que três créditos.*/
 SELECT CodDepto FROM Disciplina WHERE CreditosDisc > 3;
 SELECT NomeDepto FROM Depto WHERE CodDepto IN (1, 2, 3);
